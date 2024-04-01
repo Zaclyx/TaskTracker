f08c43e4477633b8dacfa9dd58a2b0c11d742b45
You Xiang <youxiang.liew@ncs.com.sg>
2024-04-01 23:54:12 +0800

    Trying Angular Firebase Firestore API calls

--- src/app/shared.service.ts
+++ src/app/shared.service.ts
@@ -1,16 +1,49 @@
 import { Injectable } from "@angular/core";
-import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import { AngularFirestore, AngularFirestoreCollection, DocumentData, DocumentReference } from '@angular/fire/compat/firestore';
+import { Observable, from, map, mergeMap } from "rxjs";
+
+export interface Item { id: string; name: string; }
 
 @Injectable({
     providedIn: 'root'
 })
 export class SharedService {
+    
+    private itemsCollection: AngularFirestoreCollection<Item>;
+    items: Observable<Item[]>;
+
     constructor(
-        private fs: Firestore
+        private fs: AngularFirestore
     ) {}
 
-    getTasks() {
-        let tasksCollection = collection(this.fs, 'tasks');
-        return collectionData(tasksCollection, {idField:'id'})
+    getTasks(): Observable<Item[]> {
+        this.itemsCollection = this.fs.collection<Item>('tasks');
+        return this.itemsCollection.valueChanges({ idField: 'id' });
+    }
+
+    getListOfReminders(): Observable<any[]> {
+        const remindersCollection = this.fs.collection('reminders');
+        return remindersCollection.valueChanges({ idField: 'id' }).pipe(
+            map((reminders: any[]) => {
+                return reminders.map(async reminder => {
+                    const data = reminder; // Assuming reminder already contains data payload
+                    const id = reminder.id;
+                    const userRef = data.user;
+                    const customerRef = data.customer;
+                    const [userData, customerData] = await Promise.all([
+                        userRef.get().then((doc: { data: () => any; }) => doc.data()),
+                        customerRef.get().then((doc: { data: () => any; }) => doc.data())
+                    ]);
+
+                    return {
+                        id,
+                        ...data,
+                        user: userData,
+                        customer: customerData
+                    };
+                });
+            }),
+            mergeMap(promises => from(Promise.all(promises)))
+        );
     }
 }
