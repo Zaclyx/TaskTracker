7c84bb5538a76b2fb88a36a07ef68357276ae46a
You Xiang <youxiang.liew@ncs.com.sg>
2024-04-18 17:15:25 +0800

    UI changes to the app

--- src/app/pages/monthlytasks/monthlytasks.module.ts
+++ src/app/pages/monthlytasks/monthlytasks.module.ts
@@ -3,6 +3,11 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { MonthlyTasksComponent } from './monthlytasks.component';
 import { WidgetsModule } from '../../_metronic/partials';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { InlineSVGModule } from 'ng-inline-svg-2';
+import { SharedModule } from 'src/app/_metronic/shared/shared.module';
 
 @NgModule({
   declarations: [MonthlyTasksComponent],
@@ -15,6 +20,11 @@ import { WidgetsModule } from '../../_metronic/partials';
       },
     ]),
     WidgetsModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatIconModule,
+    InlineSVGModule,
+    SharedModule,
   ],
 })
 export class MonthlyTasksModule {}
