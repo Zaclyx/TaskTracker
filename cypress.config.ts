577e4866d2abf021b8ce1a3fbb1147fcd7df2a5d
Zaclyx <116730771+Zaclyx@users.noreply.github.com>
2024-04-25 02:58:21 +0800

    Create cypress.config.ts

---
+++ cypress.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+  projectId: '55vxfv', // Add projectId here
+});
